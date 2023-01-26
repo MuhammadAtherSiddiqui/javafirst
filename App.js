b2fa83fd8ba90366bb0ac49f132ccf29a8ff4276
MuhammadAtherSiddiqui <122442314+MuhammadAtherSiddiqui@users.noreply.github.com>
2023-01-26 04:26:10 -0800

    Add two file

---
+++ App.js
@@ -0,0 +1,24 @@
+// CHAPTER-----01
+// Q NO # 01
+alert("Hello");
+
+//Q NO # 02
+alert("Error! please enter a valid password.");
+
+//Q NO # 3
+alert("Welcome to JS Land.....\nHappy Codiong!");
+
+//Q NO # 04
+alert("Welcome to JS Land..."); 
+alert("Happy coding!");
+
+//Q NO # 05
+alert("Hello...I can run JS throughmy web browser's console");
+
+
+
+
+
+
+
+
